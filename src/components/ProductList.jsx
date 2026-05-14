efa94d53faab875f88f12e7119759997c386eb2d
txmzyyy <tamaramk08@gmail.com>
2026-05-14 17:40:39 +0300

    add filter functionatlity to the productlist

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,13 +7,13 @@ export const sampleProducts = [
   { id: 2, name: 'Milk', price: '$2.50', category: 'Dairy', inStock: false }
 ]
 
-const ProductList = () => {
+const ProductList = ({products , selectedCategory}) => {
   return (
     <div>
       <h2>Available Products</h2>
 
       {/* TODO: Filter sample data using selected category */}
-      {sampleProducts.map((product) => (
+      {products.filter(product => product.category === selectedCategory).map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>

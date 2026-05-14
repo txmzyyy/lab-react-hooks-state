1030898643f421f3d8b4a47b6960c29a4ba335e7
txmzyyy <tamaramk08@gmail.com>
2026-05-14 17:28:32 +0300

    implement dark mode toggle logic

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -4,7 +4,9 @@ const DarkModeToggle = () => {
   // TODO: Implement dark mode toggle logic
 
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
+    <button onClick={toggleDarkMode}>
+      {darkMode ? 'Light Mode' : 'Dark Mode'}
+    </button>
   )
 }
 

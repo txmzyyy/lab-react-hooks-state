5310b1816af678bddc02850bb41c5e70b9d75e17
txmzyyy <tamaramk08@gmail.com>
2026-05-14 17:26:29 +0300

    added cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,6 +6,9 @@ const Cart = () => {
       <h2>Shopping Cart</h2>
       <ul>
         {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+        {items.map(item => (
+          <li key={item.id}>{item.name} is in your cart.</li>
+        ))}
       </ul>
     </div>
   )

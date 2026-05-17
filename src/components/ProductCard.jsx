2fd815f17b82f4c8668399528d5fcc243df1ca93
txmzyyy <tamaramk08@gmail.com>
2026-05-17 23:55:51 +0300

    fix:productcard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import styles from '../styles/ProductCard.module.css'
 
-const ProductCard = ({ product, addToCart }) => {
+const ProductCard = ({ product, onAddToCart }) => {
   return (
-    <div
-      className={`${styles.card} ${!product.inStock ? styles.outOfStock : ''}`}
-    >
+    <div>
       <h3>{product.name}</h3>
-      <p>Price: {product.price}</p>
-      <p>Status: {product.inStock ? 'In Stock' : 'Out of Stock'}</p>
+      <p>{product.price}</p>
+      <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
 
-      {/* TODO: Implement Add to Cart button functionality */}
-      <button data-testid={'product-' + product.id}
-          onClick={() => addToCart(product)} 
-          disabled={!product.inStock}>
-          Add to Cart
-        </button>
+      <button
+        data-testid={"product-" + product.id}
+        onClick={() => onAddToCart(product)}
+      >
+        Add to Cart
+      </button>
     </div>
-  )
-}
+  );
+};
+
 
 export default ProductCard
